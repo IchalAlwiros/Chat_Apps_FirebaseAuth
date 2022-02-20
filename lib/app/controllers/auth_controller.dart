4879f146d2b3155c7f8c9e8eb6551bb230b90a41
Ichal Wira Sukmana <69945382+IchalAlwiros@users.noreply.github.com>
2022-02-20 10:06:13 +0700

    Update

--- lib/app/controllers/auth_controller.dart
+++ lib/app/controllers/auth_controller.dart
@@ -1,3 +1,138 @@
+
+// import 'package:cobacli/app/routes/app_pages.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:get/get.dart';
+// import 'package:get_storage/get_storage.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+
+// class AuthController extends GetxController {
+//   var isSkipIntro = false.obs;
+//   var isAuth = false.obs;
+
+//   GoogleSignIn _googleSignIn = GoogleSignIn();
+//   GoogleSignInAccount? _currentUser;
+//   UserCredential? userCredential;
+
+//   FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+//   Future<void> firstInitialized() async {
+//     await autoLogin().then((value) {
+//       if (value) {
+//         isAuth.value = true;
+//       }
+//     });
+
+//     await skipIntro().then((value) {
+//       if (value) {
+//         isSkipIntro.value = true;
+//       }
+//     });
+//     //Mengubah isAurh => true => autoLogin
+//     // try {
+//     //   final isSignIn = await _googleSignIn.isSignedIn();
+//     //   if (isSignIn) {
+//     //     isAuth.value = true;
+//     //   }
+//     // } catch (err) {
+//     //   print(err);
+//     // }
+
+//     //mengubah isSkipIntro => true
+//   }
+
+//   Future<bool> skipIntro() async {
+//     final box = GetStorage();
+//     if (box.read('skipIntro') != null || box.read('skipIntro') == true) {
+//       isSkipIntro.value = true;
+//     }
+//     return false;
+//   }
+
+//   Future<bool> autoLogin() async {
+// //kita akan mengubah isAuth => true => authoLogin
+//     try {
+//       final isSignIn = await _googleSignIn.isSignedIn();
+//       if (isSignIn) {
+//         return true;
+//       }
+//       return false;
+//     } catch (err) {
+//       return false;
+//     }
+//   }
+
+//   Future<void> login() async {
+//     try {
+//       // Ini untuk handle kebocoran data user sebelum login
+//       await _googleSignIn.signOut();
+
+//       //Ini digunakan untuk mendapatkan google account
+//       await _googleSignIn.signIn().then((value) => _currentUser = value);
+
+//       //ini untuk mengecek status ;pgin user
+//       final isSignin = await _googleSignIn.isSignedIn();
+
+//       if (isSignin) {
+//         // kondisi login berhasil
+//         print("SUDAH BERHASIL LOGIN DENGAN AKUN : ");
+//         print(_currentUser);
+
+//         final googleAuth = await _currentUser!.authentication;
+
+//         final credential = GoogleAuthProvider.credential(
+//           idToken: googleAuth.idToken,
+//           accessToken: googleAuth.accessToken,
+//         );
+
+//         // final userCredential =
+//         await FirebaseAuth.instance
+//             .signInWithCredential(credential)
+//             .then((value) => userCredential = value);
+
+//         print("USER CREDENTIAL");
+//         print(userCredential);
+
+//         //Simpan status user bahwa sudah pernah login & tidak akan menammpilkan introduction
+//         final box = GetStorage();
+//         if (box.read('skipIntro') != null) {
+//           box.remove('skipIntro');
+//         }
+//         box.write('skipIntro', true);
+
+//         //Masukan data ke firebase
+//         CollectionReference users = firestore.collection('users');
+//         users.doc(_currentUser!.email).set({
+//           "name": _currentUser!.displayName,
+//           "email": _currentUser!.email,
+//           "photoUrl": _currentUser!.photoUrl,
+//           "status": "",
+//           "creationTime":
+//               userCredential!.user!.metadata.creationTime!.toIso8601String(),
+//           "lastSignTime":
+//               userCredential!.user!.metadata.lastSignInTime!.toIso8601String(),
+//           "updatedTime": DateTime.now().toIso8601String(),
+//         });
+
+//         isAuth.value = true;
+//         Get.offAllNamed(Routes.HOME);
+//       } else {
+//         print("TIDAK BERHASIL LOGIN");
+//       }
+//     } catch (error) {
+//       print(error);
+//     }
+//   }
+
+//   Future<void> logout() async {
+//     await _googleSignIn.disconnect();
+//     await _googleSignIn.signOut();
+//     Get.offAllNamed(Routes.LOGIN);
+//   }
+// }
+
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cobacli/app/routes/app_pages.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
@@ -12,6 +147,8 @@ class AuthController extends GetxController {
   GoogleSignInAccount? _currentUser;
   UserCredential? userCredential;
 
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
   Future<void> firstInitialized() async {
     await autoLogin().then((value) {
       if (value) {
@@ -24,29 +161,19 @@ class AuthController extends GetxController {
         isSkipIntro.value = true;
       }
     });
-    //Mengubah isAurh => true => autoLogin
-    // try {
-    //   final isSignIn = await _googleSignIn.isSignedIn();
-    //   if (isSignIn) {
-    //     isAuth.value = true;
-    //   }
-    // } catch (err) {
-    //   print(err);
-    // }
-
-    //mengubah isSkipIntro => true
   }
 
   Future<bool> skipIntro() async {
+    // kita akan mengubah isSkipIntro => true
     final box = GetStorage();
     if (box.read('skipIntro') != null || box.read('skipIntro') == true) {
-      isSkipIntro.value = true;
+      return true;
     }
     return false;
   }
 
   Future<bool> autoLogin() async {
-//kita akan mengubah isAuth => true => authoLogin
+    // kita akan mengubah isAuth => true => autoLogin
     try {
       final isSignIn = await _googleSignIn.isSignedIn();
       if (isSignIn) {
@@ -63,13 +190,13 @@ class AuthController extends GetxController {
       // Ini untuk handle kebocoran data user sebelum login
       await _googleSignIn.signOut();
 
-      //Ini digunakan untuk mendapatkan google account
+      // Ini digunakan untuk mendapatkan google account
       await _googleSignIn.signIn().then((value) => _currentUser = value);
 
-      //ini untuk mengecek status ;pgin user
-      final isSignin = await _googleSignIn.isSignedIn();
+      // ini untuk mengecek status login user
+      final isSignIn = await _googleSignIn.isSignedIn();
 
-      if (isSignin) {
+      if (isSignIn) {
         // kondisi login berhasil
         print("SUDAH BERHASIL LOGIN DENGAN AKUN : ");
         print(_currentUser);
@@ -81,7 +208,6 @@ class AuthController extends GetxController {
           accessToken: googleAuth.accessToken,
         );
 
-        // final userCredential =
         await FirebaseAuth.instance
             .signInWithCredential(credential)
             .then((value) => userCredential = value);
@@ -89,13 +215,27 @@ class AuthController extends GetxController {
         print("USER CREDENTIAL");
         print(userCredential);
 
-        //Simpan status user bahwa sudah pernah login & tidak akan menammpilkan introduction
+        // simpan status user bahwa sudah pernah login & tidak akan menampilkan introduction kembali
         final box = GetStorage();
         if (box.read('skipIntro') != null) {
           box.remove('skipIntro');
         }
-
         box.write('skipIntro', true);
+
+        // masukan data ke firebase...
+        CollectionReference users = firestore.collection('users');
+        users.doc(_currentUser!.email).set({
+          "uid": userCredential!.user!.uid,
+          "name": _currentUser!.displayName,
+          "email": _currentUser!.email,
+          "photoUrl": _currentUser!.photoUrl,
+          "status": "",
+          "creationTime":
+              userCredential!.user!.metadata.creationTime!.toIso8601String(),
+          "lastSignInTime":
+              userCredential!.user!.metadata.lastSignInTime!.toIso8601String(),
+          "updatedTime": DateTime.now().toIso8601String(),
+        });
 
         isAuth.value = true;
         Get.offAllNamed(Routes.HOME);
@@ -113,3 +253,4 @@ class AuthController extends GetxController {
     Get.offAllNamed(Routes.LOGIN);
   }
 }
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,93 @@
+// import 'package:cobacli/app/controllers/auth_controller.dart';
+// import 'package:cobacli/app/utils/error_screen.dart';
+// import 'package:cobacli/app/utils/loading_screen.dart';
+// import 'package:cobacli/app/utils/splash_screen.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
+
+// import 'package:get/get.dart';
+// import 'package:get_storage/get_storage.dart';
+
+// import 'app/routes/app_pages.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   await GetStorage.init();
+//   runApp(_MyApp());
+// }
+
+// class _MyApp extends StatelessWidget {
+
+//   final authC = Get.put(AuthController(), permanent: true);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return FutureBuilder(
+//                 future: Future.delayed(const Duration(seconds: 3)),
+//                 builder: (context, snapshot) {
+//                   if (snapshot.connectionState == ConnectionState.done) {
+//                     return Obx(() => GetMaterialApp(
+//                           debugShowCheckedModeBanner: false,
+//                           title: "ChatApp ",
+//                           initialRoute: authC.isSkipIntro.isTrue
+//                               ? authC.isAuth.isTrue
+//                                   ? Routes.HOME
+//                                   : Routes.LOGIN
+//                               : Routes.INTRODUCTION,
+//                           getPages: AppPages.routes,
+//                         ));
+//                   }
+//                   return FutureBuilder(
+//                       future: authC.firstInitialized(),
+//                       builder: (context, snapshot) => const SplashScreen());
+//                 });
+
+       
+//   }
+// }
 import 'package:cobacli/app/controllers/auth_controller.dart';
-import 'package:cobacli/app/utils/error_screen.dart';
-import 'package:cobacli/app/utils/loading_screen.dart';
-import 'package:cobacli/app/utils/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
 import 'app/routes/app_pages.dart';
+import 'app/utils/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await GetStorage.init();
-  runApp(_MyApp());
+  runApp(MyApp());
 }
 
-class _MyApp extends StatelessWidget {
-  final Future<FirebaseApp> _initializzation = Firebase.initializeApp();
-
+class MyApp extends StatelessWidget {
   final authC = Get.put(AuthController(), permanent: true);
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: _initializzation,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return const ErrorScreen();
-          }
-
-          if (snapshot.connectionState == ConnectionState.done) {
-            // return GetMaterialApp(
-            //   debugShowCheckedModeBanner: false,
-            //   title: "App ",
-            //   initialRoute: Routes.LOGIN,
-            //   getPages: AppPages.routes,
-            // );
-            return FutureBuilder(
-                future: Future.delayed(const Duration(seconds: 3)),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    return Obx(() => GetMaterialApp(
-                          debugShowCheckedModeBanner: false,
-                          title: "ChatApp ",
-                          initialRoute: authC.isSkipIntro.isTrue
-                              ? authC.isAuth.isTrue
-                                  ? Routes.HOME
-                                  : Routes.LOGIN
-                              : Routes.INTRODUCTION,
-                          getPages: AppPages.routes,
-                        ));
-                  }
-                  return FutureBuilder(
-                      future: authC.firstInitialized(),
-                      builder: (context, snapshot) => const SplashScreen());
-                });
-          }
-          return const LoadingScreen();
-        });
+      future: Future.delayed(Duration(seconds: 3)),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Obx(
+            () => GetMaterialApp(
+              title: "ChatApp",
+              initialRoute: authC.isSkipIntro.isTrue
+                  ? authC.isAuth.isTrue
+                      ? Routes.HOME
+                      : Routes.LOGIN
+                  : Routes.INTRODUCTION,
+              getPages: AppPages.routes,
+            ),
+          );
+        }
+        return FutureBuilder(
+          future: authC.firstInitialized(),
+          builder: (context, snapshot) => SplashScreen(),
+        );
+      },
+    );
   }
 }
